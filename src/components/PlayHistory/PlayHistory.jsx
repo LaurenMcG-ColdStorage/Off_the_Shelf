bb614d2e0166c4ad05473cace74ad712b154ff4e
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-03-20 22:05:46 -0400

    session router created. PlayHistoryModal revised

--- src/components/PlayHistory/PlayHistory.jsx
+++ src/components/PlayHistory/PlayHistory.jsx
@@ -3,16 +3,29 @@ import { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 
 import './PlayHistory.css';
-import {PlayHistoryModal} from './PlayHistoryModal';
+import { PlayHistoryModal } from './PlayHistoryModal';
 
 function PlayHistory(){
 
     const dispatch = useDispatch();
     const sessionHistory = useSelector((store) => store.sessionHistory);
+    const [modalActive, setModalActive] = useState(false);
+
+    const handleClose = () => {
+        setModalActive(false);
+    };
+
+    const handleAdd = () => {
+        setModalActive(true);
+    }
 
     return(
         <div>
+            {modalActive && 
+                <PlayHistoryModal onClose={handleClose()} />
+            }
             <h2>This is the Play History Page</h2>
+            <button onClick={() => handleAdd()}>Enter Play Session</button>
         <table>
             <thead>
                 <tr>
