de2e22cf8f125deab1e7b4770351e43470a135fa
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-03-23 10:33:39 -0400

    session router GET route created

--- server/routes/session.router.js
+++ server/routes/session.router.js
@@ -3,11 +3,21 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    const getHistory = req.body;
+    const historyQuery = `SELECT * FROM "history" WHERE "user_id" = $1;`;
+    
+    pool
+    .query(historyQuery, [getHistory.id])
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        res.sendStatus(500);
+    })
 })
 
 router.post('/', (req, res) => {
-    
+
 })
 
 module.exports = router;
