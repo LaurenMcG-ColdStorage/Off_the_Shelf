e50e1c7d6472eef2f5c8342dde42f85ecd7827b9
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-03-23 10:12:33 -0400

    Updated root reducer with sessionHistory import.

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import sessionHistory from './session.reducer';
 import collection from './collection.reducer';
 import errors from './errors.reducer';
 import user from './user.reducer';
@@ -12,7 +13,8 @@ import user from './user.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id, username, collection, and role if someone is logged in
-  collection,
+  collection, //This will hold all of the games in the specified collection.
+  sessionHistory, //This will hold all of the play sessions that a user has logged.
 });
 
 export default rootReducer;
