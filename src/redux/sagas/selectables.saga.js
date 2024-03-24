eb1b1d485596cfb4dffa0a0ab6b865c39a0c522c
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-03-24 18:37:40 -0400

    selectable reducer and saga built. Router file created.

--- src/redux/sagas/selectables.saga.js
+++ src/redux/sagas/selectables.saga.js
@@ -4,14 +4,19 @@ import { takeEvery, put } from 'redux-saga/effects';
 function* getGameMechanics(){
     try {
         const mechResponse = yield axios.get(`/api/select/mech`);
-        yield put({type: 'SET_MECHANICS', payload: mechResponse.data})
+        yield put({type: 'SET_MECHANICS', payload: mechResponse.data});
     } catch (error) {
         console.log('Error getting game mechanics');
     }
 };
 
 function* getGameThemes(){
-
+    try {
+        const themeResponse = yield axios.get(`/api/select/theme`);
+        yield put ({type: 'SET_GAME_THEMES', payload: themeResponse.data});
+    } catch (error) {
+        console.log('Error getting game themes');
+    }
 };
 
 function* selectablesSaga(){
