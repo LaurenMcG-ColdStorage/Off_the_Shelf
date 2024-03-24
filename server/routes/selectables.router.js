38b77f7b124667f68af43817f488fba1fc7e7425
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-03-24 18:44:57 -0400

    Mechanics GET query built

--- server/routes/selectables.router.js
+++ server/routes/selectables.router.js
@@ -2,4 +2,20 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+router.get('/:mech', (req,res) => {
+    const mechQuery = `SELECT * FROM "mechanics";`;
+
+    pool
+    .query(mechQuery)
+    .then((result) => {
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        res.sendStatus(500);
+    })
+});
+
+router.get('/:theme', (req, res) => {
+    const themeQuery = `SELECT * FROM "themes";`;
+})
 module.exports = router;
