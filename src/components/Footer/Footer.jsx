c30f82f3090f15055efbf2dd48003516f9569ba8
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-04-09 18:55:44 -0400

    Updated Data Policy link

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
   return (
     <footer className='navFoot'>
       <div className='navBarFoot'>
-        <Link to='/data' className='navLinkFoot' sx={{color: '#f2f2f2', textDecoration: 'none'}}>Data Policy</Link>
+        <Link to='/data' className='navLinkFoot' sx={{color: '#f2f2f2', fontSize: 10, textDecoration: 'none'}}>Data Policy</Link>
         <div>&copy; Rust Monster</div>
       </div>
 
